517927443e91c856a9d9b28eb2f82158995d71d5
dlfdyd96 <1yongs_@naver.com>
2021-09-26 23:32:50 +0900

    HOTFIX: cors pattern 오타 수정

--- src/main/java/com/example/gccoffee/configuration/MvcConfiguration.java
+++ src/main/java/com/example/gccoffee/configuration/MvcConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/&&").allowedOrigins("*");
+        registry.addMapping("/api/**").allowedOrigins("*");
     }
 }

222a365758423a03a668175e3b8021313144631f
dlfdyd96 <1yongs_@naver.com>
2021-09-26 17:17:45 +0900

    FEAT: 입력페이지 처리

--- src/main/java/com/example/gccoffee/controller/ProductController.java
+++ src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.example.gccoffee.controller;
 
+import com.example.gccoffee.controller.dto.CreateProductRequest;
 import com.example.gccoffee.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
@@ -20,6 +22,18 @@ public class ProductController {
         return "product-list";
     }
 
+    @GetMapping("new-product")
+    public String newProduct() {
+        return "new-product";
+    }
 
+    @PostMapping("/products")
+    public String newProduct(CreateProductRequest createProductRequest) {
+        productService.createProduct(createProductRequest.productName(),
+                createProductRequest.category(),
+                createProductRequest.price(),
+                createProductRequest.description());
+        return "redirect:/products";
+    }
 
 }

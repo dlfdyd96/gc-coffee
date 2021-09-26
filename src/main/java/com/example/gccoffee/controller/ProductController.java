0e62047d6d340124dda345b66b8b26c39eefd31c
dlfdyd96 <1yongs_@naver.com>
2021-09-26 17:03:31 +0900

    FEAT: controller, template 작성

---
+++ src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.gccoffee.controller;
+
+import com.example.gccoffee.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String productPage(Model model) {
+        var products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+
+
+
+}
--- src/main/java/com/example/gccoffee/service/DefaultProductService.java
+++ src/main/java/com/example/gccoffee/service/DefaultProductService.java
@@ -3,11 +3,13 @@ package com.example.gccoffee.service;
 import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
 import com.example.gccoffee.repository.ProductRepository;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class DefaultProductService implements ProductService {
 
     private final ProductRepository productRepository;
